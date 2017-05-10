e28cf9c0f96f88671211e11eecb6f29f76df5cfe
Lloyd Grubham <grubham@gmail.com>
2017-05-09 22:46:22 -0600

    Deck cycle and dealt cards all have basic funtionality

--- app/routes/mainCtrl.js
+++ app/routes/mainCtrl.js
@@ -7,6 +7,7 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {
     
     $scope.discard = [];
 
+    // $scope.player1Flip = [0,0,1,0,0,1];
     $scope.player1Flip = [0,0,0,0,0,0];
     $scope.player2Flip = [0,0,0,0,0,0];
 
@@ -14,7 +15,20 @@ angular.module('app').controller('mainCtrl', function ($scope, mainSrv) {
     $scope.draw = function () {
         $scope.discard.push(mainSrv.draw($scope.deck));
         $scope.discardTop = $scope.discard[$scope.discard.length - 1];
-        $scope.deckLen = $scope.deck.length;
     }
     $scope.deckLen = $scope.deck.length;
+
+    $scope.flipDownCard = function (player, cardIndex) {
+        if (player == 1) {
+            $scope.player1Flip[cardIndex] = 1;
+        } else if (player == 2){
+            $scope.player2Flip[cardIndex] = 1;
+        }
+    }
+
+    $scope.reshuffleDiscard = function () {
+        $scope.deck = mainSrv.shufflePack($scope.discard);
+        $scope.discard = [];
+        $scope.discardTop = null;
+    }
 })
